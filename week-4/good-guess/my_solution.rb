025ef108c49fbdc61eb57230bd6e778621a23b0a
sjones88 <steven.g.jones88@gmail.com>
2015-11-12 16:14:26 -0600

    Complete good guess challenge

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,15 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def good_guess?(integer)
+	integer.to_i
+	if integer == 42
+		return true
+	else
+		return false
+	end
+end
