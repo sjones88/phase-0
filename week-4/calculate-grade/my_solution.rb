28ab6f7e7f16b7314a5ebf4f52acbfaf24bb3dda
sjones88 <steven.g.jones88@gmail.com>
2015-11-12 15:38:18 -0600

    Complete letter grade and triangle exercises

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,27 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def get_grade(num_grade)
+
+	if num_grade >= 90
+		grade = 'A'
+		p grade
+	elsif num_grade >= 80
+		grade = 'B'
+		p grade
+	elsif num_grade >= 70
+		grade = 'C'
+		p grade
+	elsif num_grade >= 60
+		grade = 'D'
+		p grade
+	else
+		grade = 'F'
+		p grade
+	end
+end
+		
---
+++ week-4/triangle-side/my_solution.rb
@@ -0,0 +1,31 @@
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def valid_triangle?(a, b, c)
+  # Your code goes here!
+  a.to_f
+  b.to_f
+  c.to_f
+  if a == 0 || b == 0 || c == 0
+  	return false
+  elsif (a + b) < c
+  	return false
+  elsif (b + c) < a
+  	return false
+  elsif (a + c) < b
+  	return false
+  else
+  	return true
+  end
+  if a == b && b == c
+  	return true
+  	puts 'True, an equilateral triangle.'
+  end
+  if (a**2 + b**2) == c**2 || (b**2 + c**2) == a**2 || (c**2 + a**2) == b**2
+  	return true
+  	puts 'True, a right triangle.'
+  end
+
+end
