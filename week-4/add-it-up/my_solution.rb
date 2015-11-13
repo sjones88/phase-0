3bf20564bbefccf279593abb05e9c46328ecd9f6
sjones88 <steven.g.jones88@gmail.com>
2015-11-13 17:36:26 -0600

    Finalize solution for challenge 4.7.2

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -9,25 +9,35 @@
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
 
-# Input: Each integer in the array
+# Input: An array of integers
 # Output: Sum of inputs
 # Steps to solve the problem.
-
+# Sum the objects in the array.
 
 # 1. total initial solution
 
-
+def total(num)
+	sum = num.inject {|x,y| x + y }
+	return sum
+end
 
 # 3. total refactored solution
 
-
+def total(num)
+	num.inject {|x,y| x + y }
+end
 
 # 4. sentence_maker pseudocode
 # make sure all pseudocode is commented out!
-# Input:
-# Output:
+# Input: An array of strings.
+# Output: A grammatically correct sequence of the strings in the array.
 # Steps to solve the problem.
-
+# Capitalize the first letter of the first string.
+# Iterate through the array:
+#   - Convert objects to strings
+#   - Add string to variable followed by a space
+# Remove last space from variable
+# Add '.' to variable string
 
 # 5. sentence_maker initial solution
 
@@ -35,9 +45,7 @@
 
 # 6. sentence_maker refactored solution
 
-def total(num)
-	num.inject {|x,y| x + y }
-end
+
 
 def sentence_maker(string)
 	string[0] = string[0].capitalize
@@ -49,3 +57,4 @@ def sentence_maker(string)
 	phrase = phrase + '.'
 	return phrase
 end
+
