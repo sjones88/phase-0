ccfc1e8c25f0c8cc30982685bf6959c453f5cf95
sjones88 <steven.g.jones88@gmail.com>
2015-11-28 15:40:16 -0600

    Complete die-2 challenge

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -2,7 +2,7 @@
 
 
 # I worked on this challenge by myself.
-# I spent [#] hours on this challenge.
+# I spent 1.5 hours on this challenge.
 
 # Pseudocode
 =begin
@@ -58,7 +58,33 @@ def die_rolls(number_of_rolls, array)
 end
 array = ['Alfred','Mr. Bond','Goldfinger','Blofeld','Jaws','Spectre','Q']
 p die_rolls(1000,array)
+
+
 # Refactored Solution
+
+class Die
+  def initialize(labels)
+    @labels = labels
+    @sides = sides
+      unless labels.size > 0
+        raise ArgumentError.new('Array is empty. Pass a non-empty array.')
+      end
+      unless labels.is_a?(Array) == true
+        raise ArgumentError.new('Please pass an array.')
+      end
+    end
+
+  def sides
+    @sides = @labels.size
+  end
+
+  def roll
+    prng = Random.new
+    die_roll = prng.rand(0...@sides)
+    @labels[die_roll]
+  end
+end
+
 =begin
 class Die
   include Enumerable	
@@ -95,16 +121,25 @@ end
 =begin
 - What were the main differences between this die class and the last one you created in terms of implementation?
   Did you need to change much logic to get this to work?
-
+There are two main differences between this die class and the first one. The sides method for this class calls the size method
+on the input array in order to return the number of sides. The roll method implements a random number generator in the same
+way that the original die class did except this time the method uses the rng output to select a random element from the array.
+The logic is very similar to the original die class.
 
 - What does this exercise teach you about making code that is easily changeable or modifiable? 
-
+I learned that making code that is easily modifiable can save time and effort. I was able to adapt my original die
+class to accept an array instead of an integer, which made this challenge much easier than it would have been if I had
+needed to start from scratch. In fact, I had time to experiment with creating a method that rolls the die a number of times
+and outputs a hash of the results. It was fun to roll the die 10,000, 100,000 and a million times and see how much longer 
+the computer took to process each increment. I wouldn't advise rolling more than a million times!
 
 - What new methods did you learn when working on this challenge, if any?
-
+I didn't need to implement any new methods.
 
 - What concepts about classes were you able to solidify in this challenge?
-
+This challenge helped highlight the power of instance variables in classes. I only needed two instance variables to get
+the sides and roll methods to work, and I was able to create an additional method that rolls the die any given number of times
+without having to create an additional instance variable.
 
 
 
