ed2cc9efafe0189996e4e6f1db0fcd9930f4034d
sjones88 <steven.g.jones88@gmail.com>
2015-11-28 17:08:11 -0600

    Work on guessing game

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -85,13 +85,15 @@ end
 # Reflection
 =begin
 - How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
-
+Instance variables endow classes with the ability to remember the state of a given object. To use an example from this challenge, the @answer and @guess instance variables
+enable instances of the GuessingGame class to 'remember' the answer to the game and the most recent guess. The methods can access the 'remembered' information and do things
+with that information. For example, the guess method remembers a guess and compares it to the answer that was passed to the instance of GuessingGame when it was initialized.
 
 - When should you use instance variables? What do they do for you?
-
+You can only use instance variables within a class that you've created. Within their class, instance variables give you an easy way to pass information between methods.
 
 - Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
-
+Flow control is the use of conditional statements within a method to allow the method to take different courses of action depending on the input that the method receives.
 
 - Why do you think this code requires you to return symbols? What are the benefits of using symbols?
 
